77d0f460a8fa21f5137e243b0820f5389b530a6b
Ezziu <arzetelski@gmail.com>
2019-04-15 16:31:47 +0200

    second push

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1a718f053bb39080bff0aa721f11ec4a.js"
+  "/Piolun_WWW/precache-manifest.b31f1b1a01bdf1fd76ca16adfd55502a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Piolun_WWW/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

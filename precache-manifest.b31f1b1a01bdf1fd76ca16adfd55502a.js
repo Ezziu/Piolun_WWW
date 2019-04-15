f946404638ee6e03f29e79ddb3b02777f3318efa
Ezziu <arzetelski@gmail.com>
2019-04-15 16:41:19 +0200

    Updates

---
+++ precache-manifest.b31f1b1a01bdf1fd76ca16adfd55502a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "13792df461e19f64bdc0d8b22c4e8419",
+    "url": "/Piolun_WWW/static/media/patronaty.13792df4.png"
+  },
+  {
+    "revision": "159b8bde93cea260903216b2d3cd1e14",
+    "url": "/Piolun_WWW/static/media/header.159b8bde.png"
+  },
+  {
+    "revision": "d51808d54caf31af71b8c2e237839d27",
+    "url": "/Piolun_WWW/static/media/background_1.d51808d5.jpg"
+  },
+  {
+    "revision": "ec6901e18da151c178cd9bb3e817c5e9",
+    "url": "/Piolun_WWW/static/media/Logo-main.ec6901e1.png"
+  },
+  {
+    "revision": "4cef5419bdf1b5f63b4c",
+    "url": "/Piolun_WWW/static/js/runtime~main.4cef5419.js"
+  },
+  {
+    "revision": "81968ba513598cd03706",
+    "url": "/Piolun_WWW/static/js/main.81968ba5.chunk.js"
+  },
+  {
+    "revision": "63dca2d42dfaba3fb212",
+    "url": "/Piolun_WWW/static/js/2.63dca2d4.chunk.js"
+  },
+  {
+    "revision": "81968ba513598cd03706",
+    "url": "/Piolun_WWW/static/css/main.090418fb.chunk.css"
+  },
+  {
+    "revision": "63dca2d42dfaba3fb212",
+    "url": "/Piolun_WWW/static/css/2.87156f0d.chunk.css"
+  },
+  {
+    "revision": "b8e5fdf194a22d3f3042c8677e6694a6",
+    "url": "/Piolun_WWW/index.html"
+  }
+];
